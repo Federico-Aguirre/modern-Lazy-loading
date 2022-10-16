c11c484a4fccfa57db73c58dd76f24a47c13757b
Federico-Aguirre <sedent333@gmail.com>
2022-10-16 14:18:20 -0300

    initial commit

---
+++ modern-lazy-loading/main.js
@@ -0,0 +1,14 @@
+if ('loading' in HTMLImageElement.prototype) {
+    //Native Lazy Loading Supported!
+
+    const images = document.querySelectorAll('img[loading="lazy"]')
+    images.forEach(img => {
+        img.src = img.dataset.src;
+    })
+} else {
+    // Not supported!
+
+    const script = document.createElement('script')
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js';
+    document.body.appendChild(script);
+}
